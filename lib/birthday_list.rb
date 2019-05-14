fff4fa65791e4fd7fb9a81d0868a4b970298608e
Anna <annas.wieclaw@gmail.com>
2019-05-14 12:24:42 +0100

    TDD'ed birthday program

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,20 @@
+class Birthday_list
+  def initialize
+    @storage = [{"Alison"=> "05-05-1900"}, {"Bob" => "13-01-2000"}, {"Mary Poppin" => "09-09-1819"}]
+  end
 
+  def show
+    @storage
+  end
+
+  def whose_birthday(date)
+    @storage.each { |hash|
+      if hash.values.join == date
+        year = (hash.values.join).split("-")
+        year = year[2].to_i
+        text = "It's #{hash.keys.join}'s birthday today! They are #{2019 - year} years old!"
+        return text
+      end
+    }
+  end
+end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,19 @@
+require "Birthday_list"
+describe "Birthday_list" do
 
+  let(:list) {
+    Birthday_list.new
+  }
+  it "can store my friends' birthdays" do
+
+    expect(list).not_to be_nil
+  end
+
+  it "can display my friends' names and birthdays" do
+    expect(list.show).to include({"Bob" => "13-01-2000"}) 
+  end
+
+  it "can check whose birthday it is today" do
+    expect(list.whose_birthday("09-09-1819")).to include("It's Mary Poppin's birthday today! They are 200 years old!")
+  end
+end
