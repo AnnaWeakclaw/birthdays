4a4a5167c2efacb0ba89a24fe3b0c223adfb3944
Anna <annas.wieclaw@gmail.com>
2019-05-15 13:55:58 +0100

    TDD birthday functionality to check todays date

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,20 +1,46 @@
+require 'date'
 class Birthday_list
+  attr_reader :storage
+
   def initialize
-    @storage = [{"Alison"=> "05-05-1900"}, {"Bob" => "13-01-2000"}, {"Mary Poppin" => "09-09-1819"}]
+    @storage = [{ "Alison" => "15-05-1900" }, { "Bob" => "13-01-2000" }, { "Mary Poppin" => "09-09-1819" }]
   end
 
   def show
-    @storage
+    display = []
+    @storage.map {|hash|
+    display.push("Hey #{hash.keys.join}, your birthday is #{hash.values.join}")
+    }
+    display
   end
 
-  def whose_birthday(date)
+  def whose_birthday
+    today = convert_today
+    today = today.split("-")
+    text = []
     @storage.each { |hash|
-      if hash.values.join == date
-        year = (hash.values.join).split("-")
-        year = year[2].to_i
-        text = "It's #{hash.keys.join}'s birthday today! They are #{2019 - year} years old!"
-        return text
+        exact_date = (hash.values.join).split("-")
+        if exact_date[0] == today[0] && exact_date[1] == today[1]
+        year = exact_date[2].to_i
+        text.push("It's #{hash.keys.join}'s birthday today! They are #{2019 - year} years old!")
       end
     }
+    text
+  end
+
+  def add(name, date)
+    new_person = { name => date }
+    @storage.push(new_person)
+  end
+
+  def convert_today
+    today = Date.today
+    date = today.to_s.split("-")
+    convert_date = []
+    convert_date.push(date[2])
+    convert_date.push(date[1])
+    convert_date.push(date[0])
+    convert_date = convert_date.join("-")
+    convert_date
   end
 end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,19 +1,26 @@
+require "date"
 require "Birthday_list"
 describe "Birthday_list" do
-
   let(:list) {
     Birthday_list.new
   }
   it "can store my friends' birthdays" do
-
     expect(list).not_to be_nil
   end
 
   it "can display my friends' names and birthdays" do
-    expect(list.show).to include({"Bob" => "13-01-2000"}) 
+    expect(list.show).to include("Hey Bob, your birthday is 13-01-2000")
   end
 
-  it "can check whose birthday it is today" do
-    expect(list.whose_birthday("09-09-1819")).to include("It's Mary Poppin's birthday today! They are 200 years old!")
+  it "can add a person and their DOB" do
+    list.add("John", "12-03-2019")
+    expect(list.storage).to include({ "John" => "12-03-2019" })
+  end
+
+  it "knows today's date and checks if it is someones birthday" do
+    date = Date.today
+    convert_date = list.convert_today
+    list.add("Emma", convert_date)
+    expect(list.whose_birthday).to include("It's Emma's birthday today! They are 0 years old!")
   end
 end
